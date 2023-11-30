ccac2f843a078b1d6af6acb5a74f176e178ff233
tferrar0 <146215589+tferrar0@users.noreply.github.com>
2023-11-30 05:39:48 +0000

    youngest and eldest

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -13,4 +13,44 @@ class DirectorsController < ApplicationController
     render({ :template => "director_templates/details"})
   end
 
+  def youngest
+   
+  latest_dob = Director.maximum(:dob)
+
+  matching_directors = Director.where({ :dob => latest_dob})
+
+  youngest_director = matching_directors.at(0)
+  
+  director_name = youngest_director.name
+
+  @youngest_name = director_name
+
+  young_birthdate = youngest_director.dob
+
+  @youngest_date = young_birthdate.strftime("%B %d, %Y")
+
+
+    render({ :template => "director_templates/youngest"})
+  end
+
+def eldest
+  latest_dob = Director.minimum(:dob)
+
+  old_matching_directors = Director.where({ :dob => latest_dob})
+
+  eldest_director = old_matching_directors.at(0)
+  
+  eldest_director_name = eldest_director.name
+
+  @eldest_name = eldest_director_name
+
+  old_birthdate = eldest_director.dob
+
+  @eldest_date = old_birthdate.strftime("%B %d, %Y")
+
+
+    render({ :template => "director_templates/eldest"})
+end
+
+
 end
